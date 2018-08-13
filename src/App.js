7f622b4f84daf1a3b06ca3b96d35bd520a4832f8
Min Suk Kim <minkimcodes@gmail.com>
2018-08-13 13:28:36 -0400

    form logs weather json

--- src/App.js
+++ src/App.js
@@ -6,18 +6,20 @@ import Weather from './components/Weather';
 const API_KEY = "24074fbe58384e2412a51ad2b43b1976";
 
 class App extends React.Component {
-  getWeather = async () => {
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}&unites=metric`);
+
+  getWeather = async (e) => {
+    e.preventDefault();
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
     console.log(data);
-    
+
   }
 
   render() {
     return(
       <div>
         <Titles />
-        <Form />
+        <Form getWeather={this.getWeather} />
         <Weather />
       </div>
     )
