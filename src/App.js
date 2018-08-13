b03b18dbc9866f70dce03821c9e2ddd9a1caf687
Min Suk Kim <minkimcodes@gmail.com>
2018-08-13 13:07:40 -0400

    added api call to weather app and form component has a mock form

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import Titles from './components/Titles';
 import Form from './components/Form';
 import Weather from './components/Weather';
 
+const API_KEY = "24074fbe58384e2412a51ad2b43b1976";
+
 class App extends React.Component {
+  getWeather = async () => {
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}&unites=metric`);
+    const data = await api_call.json();
+    console.log(data);
+    
+  }
+
   render() {
     return(
       <div>
