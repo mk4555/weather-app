c84081e913d7c7f8db754407874ff4dcdab1d6b7
Min Suk Kim <minkimcodes@gmail.com>
2018-08-14 13:11:21 -0400

    data shows only when available

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,7 +4,11 @@ class Weather extends React.Component {
   render() {
     return(
       <div>
-        Weather Component
+        { this.props.city && this.props.country && <p>Location: {this.props.temperature}, {this.props.country}</p> }
+        { this.props.temperature && <p>Temperature: {this.props.temperature} C</p> }
+        { this.props.humidity && <p>Humidity: {this.props.humidity}</p> }
+        { this.props.description && <p>Conditions: {this.props.description}</p> }
+        { this.props.error && <p>{ this.props.error }</p> }
       </div>
     )
   }
